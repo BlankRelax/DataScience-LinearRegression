2c21581a71c200997984f1062220e524ae0e03e1
Hassaan Mirza <84412764+BlankRelax@users.noreply.github.com>
2022-07-04 16:42:18 +0100

    correlations

--- R_HR_BB_analysis.R
+++ R_HR_BB_analysis.R
@@ -1,7 +1,8 @@
 library(Lahman)
 library(tidyverse)
-install.packages("HistData")
 #R = runs 
 #HR = homeruns
-#Teams %>% filter(yearID %in% 1961:2001) %>% mutate(HR_per_game = HR/G, R_per_game = R/G) %>% ggplot(aes(HR_per_game, R_per_game)) + geom_point(alpha = 0.5) # HOME RUNS VS RUNS
-Teams %>% filter(yearID %in% 1961:2001) %>% mutate(X3B_per_game = X3B/G, X2B_per_game = X2B/G) %>% ggplot(aes(X2B_per_game, X3B_per_game)) + geom_point(alpha = 0.5) # STOLEN BASES VS RUNS
+Teams %>% filter(yearID %in% 1961:2001) %>% mutate(B_per_game = B/G, R_per_game = R/G) %>% ggplot(aes(B_per_game, R_per_game)) + geom_point(alpha = 0.5) # HOME RUNS VS RUNS
+#Teams %>% filter(yearID %in% 1961:2001) %>% mutate(X3B_per_game = X3B/G, X2B_per_game = X2B/G) %>% ggplot(aes(X2B_per_game, X3B_per_game)) + geom_point(alpha = 0.5) # STOLEN BASES VS RUNS
+Teams %>% filter(yearID %in% 1961:2001) %>% mutate(W_per_game = W/G, E_per_game = E/G) %>% summarise(cor(W_per_game, E_per_game))
+Teams %>% filter(yearID %in% 1961:2001) %>% mutate(X3B_per_game = X3B/G, X2B_per_game = X2B/G) %>% summarise(cor(X3B_per_game, X2B_per_game))
--- galtonfamilies.R
+++ galtonfamilies.R
@@ -1,7 +1,21 @@
 library(HistData)
 library(tidyverse)
-library()
 data("GaltonFamilies")
 
 galton_heights <-GaltonFamilies %>% filter(childNum ==1 & gender == "male") %>% select(father, childHeight) %>% rename(son = childHeight) 
 galton_heights %>% summarise(cor(father, son))
+
+R <- sample_n(galton_heights, 25, replace = TRUE) %>% summarise(cor(father, son)) # small sample of parent sample
+R
+
+#monte carlo simulation
+
+B <- 1000
+N <- 50
+R <- replicate(B, {
+  sample_n(galton_heights, N, replace = TRUE) %>% summarise(r = cor(father, son)) %>% .$r
+})
+
+#data.frame(R) %>% ggplot(aes(R)) + geom_histogram(binwidth = 0.05, color = "black")
+mean(R)
+sd(R)
