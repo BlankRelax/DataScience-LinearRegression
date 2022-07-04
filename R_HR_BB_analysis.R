59a3924fbc3f084d1b0c7730c2f20db91d511d9e
Hassaan Mirza <84412764+BlankRelax@users.noreply.github.com>
2022-07-04 14:48:22 +0100

    adapating to other varibes e.g. X2B

--- R_HR_BB_analysis.R
+++ R_HR_BB_analysis.R
@@ -3,5 +3,4 @@ library(tidyverse)
 #R = runs 
 #HR = homeruns
 #Teams %>% filter(yearID %in% 1961:2001) %>% mutate(HR_per_game = HR/G, R_per_game = R/G) %>% ggplot(aes(HR_per_game, R_per_game)) + geom_point(alpha = 0.5) # HOME RUNS VS RUNS
-Teams %>% filter(yearID %in% 1961:2001) %>% mutate(SB_per_game = SB/G, R_per_game = R/G) %>% ggplot(aes(SB_per_game, R_per_game)) + geom_point(alpha = 0.5) # STOLEN BASES VS RUNS
-data("Teams")
+Teams %>% filter(yearID %in% 1961:2001) %>% mutate(X3B_per_game = X3B/G, X2B_per_game = X2B/G) %>% ggplot(aes(X2B_per_game, X3B_per_game)) + geom_point(alpha = 0.5) # STOLEN BASES VS RUNS
