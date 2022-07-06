edb9266133ff4e3486d5aae8a2977f38874cd1e1
Hassaan Mirza <84412764+BlankRelax@users.noreply.github.com>
2022-07-06 16:49:55 +0100

    lm for linear model and stratiding HR to remove the confounding variabe(a variable that is dependant on both x and y)

--- R_HR_BB_analysis.R
+++ R_HR_BB_analysis.R
@@ -1,8 +1,13 @@
 library(Lahman)
 library(tidyverse)
-#R = runs 
-#HR = homeruns
-Teams %>% filter(yearID %in% 1961:2001) %>% mutate(B_per_game = B/G, R_per_game = R/G) %>% ggplot(aes(B_per_game, R_per_game)) + geom_point(alpha = 0.5) # HOME RUNS VS RUNS
+library(caret)
+data("Teams")
+
+
+#Teams %>% filter(yearID %in% 1961:2001) %>% mutate(B_per_game = B/G, R_per_game = R/G) %>% ggplot(aes(B_per_game, R_per_game)) + geom_point(alpha = 0.5) # HOME RUNS VS RUNS
 #Teams %>% filter(yearID %in% 1961:2001) %>% mutate(X3B_per_game = X3B/G, X2B_per_game = X2B/G) %>% ggplot(aes(X2B_per_game, X3B_per_game)) + geom_point(alpha = 0.5) # STOLEN BASES VS RUNS
-Teams %>% filter(yearID %in% 1961:2001) %>% mutate(W_per_game = W/G, E_per_game = E/G) %>% summarise(cor(W_per_game, E_per_game))
-Teams %>% filter(yearID %in% 1961:2001) %>% mutate(X3B_per_game = X3B/G, X2B_per_game = X2B/G) %>% summarise(cor(X3B_per_game, X2B_per_game))
+T#eams %>% filter(yearID %in% 1961:2001) %>% mutate(W_per_game = W/G, E_per_game = E/G) %>% summarise(cor(W_per_game, E_per_game))
+Teams %>% filter(yearID %in% 1961:2001) %>% mutate(X3B_per_game = X3B/G, X2B_per_game = X2B/G) %>% summarise(cor(X3B_per_game, X2B_per_game)) 
+
+dat <- Teams %>% filter(yearID %in% 1961:2001) %>% mutate(HR_strata = round(HR/G,1), BB_per_game = BB/G, R_per_game = R/G) %>% filter(HR_strata >= 0.4 & HR_strata <=1.2)
+dat %>% ggplot(aes(BB_per_game, R_per_game)) + geom_point(alpha = 0.5) + geom_smooth(method = "lm")# + facet_wrap(~HR_strata)
--- galtonfamilies.R
+++ galtonfamilies.R
@@ -3,19 +3,21 @@ library(tidyverse)
 data("GaltonFamilies")
 
 galton_heights <-GaltonFamilies %>% filter(childNum ==1 & gender == "male") %>% select(father, childHeight) %>% rename(son = childHeight) 
-galton_heights %>% summarise(cor(father, son))
+#galton_heights %>% summarise(cor(father, son))
+lm(son ~ father, data = galton_heights)
 
-R <- sample_n(galton_heights, 25, replace = TRUE) %>% summarise(cor(father, son)) # small sample of parent sample
-R
+#R <- sample_n(galton_heights, 25, replace = TRUE) %>% summarise(cor(father, son)) # small sample of parent sample
+#R
 
 #monte carlo simulation
 
-B <- 1000
-N <- 50
-R <- replicate(B, {
-  sample_n(galton_heights, N, replace = TRUE) %>% summarise(r = cor(father, son)) %>% .$r
-})
+#B <- 1000
+#N <- 50
+#R <- replicate(B, {
+ # sample_n(galton_heights, N, replace = TRUE) %>% summarise(r = cor(father, son)) %>% .$r
+#})
 
 #data.frame(R) %>% ggplot(aes(R)) + geom_histogram(binwidth = 0.05, color = "black")
-mean(R)
-sd(R)
+#mean(R)
+#sd(R) 
+
