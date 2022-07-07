9123befaa3ee34b56f8ec5fcb19ca342628bd37f
Hassaan Mirza <84412764+BlankRelax@users.noreply.github.com>
2022-07-07 11:41:04 +0100

    residual sum of squares

--- galtonfamilies.R
+++ galtonfamilies.R
@@ -21,3 +21,13 @@ lm(son ~ father, data = galton_heights)
 #mean(R)
 #sd(R) 
 
+rss <- function(beta0, beta1, data){
+  resid <- galton_heights$son - (beta0+beta1*galton_heights$father)
+  return(sum(resid^2))
+}
+
+beta1 = seq(0, 1, len=nrow(galton_heights))
+results <- data.frame(beta1 =  beta1, rss = sapply(beta1, rss, beta0 = 25))
+
+results %>% ggplot(aes(beta1, rss)) + geom_line() + geom_line(aes(beta1, rss), col=2)
+results
